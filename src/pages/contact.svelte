52a521eadc3bb5315197ce5da685d2d7bdfb9f1f
marciofao <marcio.lopes.fao@gmail.com>
2023-04-01 14:51:22 -0300

    first commit

---
+++ src/pages/contact.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { metatags } from "@roxi/routify";
+
+  metatags.title = "Get in touch";
+  metatags.description = "Get in touch";
+</script>
+
+<h1>Get in Touch</h1>
+
+<p>Follow me around the web</p>
